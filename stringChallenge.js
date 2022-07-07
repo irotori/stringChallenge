5c3033f0cde33d40143ef94e7345136c5a47d6ba
baroka <birotori@gmail.com>
2022-07-07 11:39:03 +0100

    Solution 2

--- stringChallenge.js
+++ stringChallenge.js
@@ -1,29 +1,23 @@
 function stringChallenge(str){
-  if(str.length === 1) return str.length;
-  if(str.split(str[0]).join('') === '') return str.length;
-  if(str.length % 2 != 0  & str[0] === str[1]) {
-      str = str.split('').reverse().join('');
-  }
-  const dic = {
+    let string = str;
+    if(string.split(string[0]).join('') === '' || string.length === 1) 
+        return string.length;
+    const dic = {
       ac: "b",
       ab: "c",
       ba: "c",
       bc: "a",
       ca: "b",
       cb: "a"
-  };
-  const reducerArr = [];
+    };
+   
+    for( let i = 0; i < string.length; i++ ) {
+      const slice = string.slice(i,i+2);
+      const match = dic[slice] || slice;
+      string = string.replace(slice,match);
+    }
 
-  for (let i=0; i<str.length; i += 2 ) {
-      let slice = str.slice(i,i+2)
-      if(dic[slice]){
-          reducerArr.push(dic[slice])
-      } else {
-          reducerArr.push(slice)
-      }
-  }
-  str = reducerArr.join('');
-  return stringChallenge(str);
+    return stringChallenge(string);
 }
 
 module.exports = stringChallenge;
